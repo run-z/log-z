a90582375efe6fcb07df75f19da9051fe9d455e4
Ruslan Lopatin <ruslan.lopatin@gmail.com>
2022-09-10 11:53:43 +0700

    Upgrade deps

--- jest.config.js
+++ jest.config.js
@@ -35,10 +35,13 @@ export default {
     ],
   ],
   testEnvironment: 'node',
-  globals: {
-    'ts-jest': {
-      tsconfig: 'tsconfig.spec.json',
-      useESM: true,
-    },
+  transform: {
+    '^.+\\.tsx?$': [
+      'ts-jest',
+      {
+        tsconfig: 'tsconfig.spec.json',
+        useESM: true,
+      },
+    ],
   },
 };
